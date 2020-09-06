79e9ebbc8eca8fff4d8082e6e6a04456c949d822
youtiaoguagua <youtiaoguagua@gmail.com>
2020-09-06 10:57:25 +0800

    找出数组中重复的数字

--- src/main/java/sword/offer/FindRepeatNumber.java
+++ src/main/java/sword/offer/FindRepeatNumber.java
@@ -1,6 +1,8 @@
 package sword.offer;
 
 
+import java.util.HashSet;
+
 /**
  * 找出数组中重复的数字。
  * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，
@@ -10,5 +12,21 @@ package sword.offer;
  * 输出：2 或 3 
  */
 public class FindRepeatNumber {
+    public static void main(String[] args) {
+        FindRepeatNumber findRepeatNumber = new FindRepeatNumber();
+        int repeatNumber = findRepeatNumber.findRepeatNumber(new int[]{2, 3, 1, 0, 2, 5, 3});
+        System.out.println(repeatNumber);
+    }
+
+    public int findRepeatNumber(int[] nums) {
+        HashSet<Integer> integers = new HashSet<>();
+        for (int num : nums) {
+            boolean add = integers.add(num);
+            if (!add){
+                return num;
+            }
+        }
+        return -1;
+    }
 
 }
