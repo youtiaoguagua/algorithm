a8e20188a2351eee1f4d3066389d104850b034cf
youtiaoguagua <youtiaoguagua@gmail.com>
2020-09-06 20:47:48 +0800

    update

---
+++ src/main/java/sword/offer/BuildTree.java
@@ -0,0 +1,71 @@
+package sword.offer;
+
+import java.util.HashMap;
+
+/**
+ * @author youtiaoguagua
+ * @date 2020/9/6 下午 6:35
+ */
+public class BuildTree {
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+    private int[] preorder;
+    private int[] inorder;
+
+    /**
+     * 输入一棵二叉树前序遍历和中序遍历的结果，请重建该二叉树。
+     * 注意:
+     * 二叉树中每个节点的值都互不相同；
+     * 输入的前序遍历和中序遍历一定合法；
+     * 样例
+     * 给定：
+     * 前序遍历是：[3, 9,5,6,4,8, 20, 15, 7]
+     * 中序遍历是：[6,5,4,9,8,3,15,20,7]
+     * 返回：[3, 9, 20, null, null, 15, 7, null, null, null, null]
+     * 返回的二叉树如下所示：
+     *      3
+     *    /   \
+     *   9    20
+     *  /  \  /  \
+     *  5   8  15  7
+     * / \
+     * 6  4
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.preorder = preorder;
+        this.inorder = inorder;
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        TreeNode root = dfs(0, preorder.length - 1, 0, inorder.length - 1);
+        return root;
+    }
+
+    private TreeNode dfs(int preLeft, int preRight, int inLeft, int inRight) {
+        if (preLeft > preRight) {
+            return null;
+        }
+
+        TreeNode treeNode = new TreeNode(preorder[preLeft]);
+        int tmp = map.get(preorder[preLeft]);
+        //前序遍历是：[3, 9,5,6,4,8, 20, 15, 7]
+        //中序遍历是：[6,5,4,9,8,3,15,20,7]
+        TreeNode left = dfs(preLeft + 1, preLeft + tmp - inLeft, inLeft, tmp - 1);
+        TreeNode right = dfs(preLeft + tmp - inLeft + 1, preRight, tmp + 1, inRight);
+        treeNode.left = left;
+        treeNode.right = right;
+        return treeNode;
+    }
+
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
